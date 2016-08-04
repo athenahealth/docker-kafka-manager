c9fafedf61fdc70637f1d275474af563bc6a50d4
Stas Alekseev <stas.alekseev@gmail.com>
2016-08-04 13:00:41 -0400

    Log only to stdout

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ZK_HOSTS=${ZK_HOSTS:-"localhost:2181"}
-APPLICATION_SECRET=${APPLICATION_SECRET:-"letmein"}
-KM_CONFIGFILE=${ZK_HOSTS:-"conf/application.conf"}
-KM_ARGS=${KM_ARGS:-"-Djava.net.preferIPv4Stack=true"}
+export ZK_HOSTS=${ZK_HOSTS:-"localhost:2181"}
+export APPLICATION_SECRET=${APPLICATION_SECRET:-"letmein"}
+export KM_CONFIGFILE=${KM_CONFIGFILE:-"conf/application.conf"}
+export CONSUMER_PROPERTIES_FILE=${CONSUMER_PROPERTIES_FILE:-"conf/consumer.properties"}
+export KM_ARGS=${KM_ARGS:-"-Djava.net.preferIPv4Stack=true"}
 
 exec ./bin/kafka-manager -Dconfig.file=${KM_CONFIGFILE} "${KM_ARGS}" "${@}"
